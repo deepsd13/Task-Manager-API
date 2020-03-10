3fd4cdb07f7ef57143c6b26b26f07c69fba9e176
Deep Shah <shah98@sheridancollege.ca>
2020-03-10 10:33:36 -0400

    Init Commit

---
+++ src/emails/account.js
@@ -0,0 +1,27 @@
+const sgMail = require('@sendgrid/mail')
+
+const sendgridAPIKey = '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'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'shah98@sheridancollege.ca',
+        subject: 'Welcome to the App',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+const sendCancellationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'shah98@sheridancollege.ca',
+        subject: 'Sorry to see you go! ',
+        text: `Goodbye,${name}. Hope to see you back soon.Is there anything we could have done to keep you on board.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancellationEmail
+}
